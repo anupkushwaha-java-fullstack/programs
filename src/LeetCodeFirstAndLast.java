23d440db28eeeac830dfc57588c399d9587f6a3f
anupkushwaha <kushwahaanup1992@gmail.com>
2024-06-22 15:51:02 +0530

    LeetCode First And Last

--- src/LeetCodeFirstAndLast.java
+++ src/LeetCodeFirstAndLast.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class LeetCodeFirstAndLast {
 
     public static void main(String[] args) {
         int[] arr = {5,7,7,7,8,8,9,8};
         int[] answer = searchRange(arr,7);
-        System.out.println(" answer::: " + answer);
+        System.out.println(" answer::: " + Arrays.toString(answer));
     }
 
     public static int [] searchRange (int[] arr, int target) {
@@ -15,26 +17,24 @@ public class LeetCodeFirstAndLast {
         return ans;
     }
 
-    public static int search(int[] arr, int target,boolean findStartIndex) {
-
-        int start = 0;
-        int end = arr.length-1;
+   static int search(int[] arr, int target, boolean findStartIndex) {
         int ans = -1;
-
+        int start = 0;
+        int end = arr.length - 1;
         while(start <= end) {
             int mid = start + (end - start) / 2;
 
-            if(target < arr[mid]){
-                end = mid -1;
-            } else if(target > arr[mid]){
-                start = mid +1;
-            }else {
-                 mid = ans;
-                 if(findStartIndex){
-                     end = mid - 1;
-                 }else{
-                     start = mid + 1;
-                 }
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
         return ans;
